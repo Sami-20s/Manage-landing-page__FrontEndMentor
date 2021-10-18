48039b21634724cb4695c291b9496f67fc64bfa9
Sami-20s <samikfouri22@gmail.com>
2021-10-18 22:07:35 +0300

    🎗 Adding Style To Index File

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./style/style.scss";
+import ContextProvider from "./context";
 
 ReactDOM.render(
     <React.StrictMode>
