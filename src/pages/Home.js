d9a3be9dd5badb70f6b28591c911661266ef3713
Sami-20s <samikfouri22@gmail.com>
2021-10-18 21:56:23 +0300

    🏠 Adding Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/Nav/Nav";
+
+const Home = () => {
+    return (
+        <>
+            <main>
+                <Nav />
+            </main>
+        </>
+    );
+};
+
+export default Home;
